c0f99b1083d6312333e1dd85d1b67171d1aa7a17
jiaming <jiaminglim0828@gmail.com>
2021-08-11 21:40:34 +0800

    test popcat

--- page_object/Home.js
+++ page_object/Home.js
@@ -1,39 +1,11 @@
 
 module.exports = {
-  url : 'http://localhost:8080/',
+  url : 'https://popcat.click/',
   elements:{
-    create_button:{
+    cat:{
       locateStrategy: 'xpath',
-      selector: '//button[text()="Create"]'
-    },
-    create_a:  {
-      locateStrategy: 'xpath',
-      selector: '//a[contains(text(),"Create")]',
-    },
-    clear_button:  {
-      locateStrategy: 'xpath',
-      selector: '//button[contains(text(),"Clear")]'
-    },
-    create_wallet_input:  {
-      locateStrategy: 'xpath',
-      selector: '//input[contains(@placeholder,"Wallet Name")]'
-    },
-    enter_password_input:  {
-      locateStrategy: 'xpath',
-      selector: '//input[contains(@placeholder,"Enter a New Password")]'
-    },
-    confirm_password_input:  {
-      locateStrategy: 'xpath',
-      selector: '//input[contains(@placeholder,"Confirm New Password")]'
-    },
-    Show_Private_Key_a:  {
-      locateStrategy: 'xpath',
-      selector: '//a[contains(text(),"Show Private Key")]',
-    },
-    Continue_a:  {
-      locateStrategy: 'xpath',
-      selector: '//a[contains(text(),"Continue")]',
-    },
+      selector: '//*[@id="app"]'
+    }
   }
 
 }
--- test/Home_test.js
+++ test/Home_test.js
@@ -2,32 +2,13 @@ var password_1 = "abcd1234";
 var password_2 = "11111111";
 var password_3 = "11111";
 module.exports = {
-  "create-wallet": function (browser) {
+  "spam_click": function (browser) {
     var signinvar = browser.page.Home();
     signinvar
       .navigate()
-      .pause(1000)
-      .click("@create_a")
-      .pause(1000)
-      .click("@create_a")
-      .pause(1000)
-      .setValue("@create_wallet_input", "Wallet Test")
-      .setValue("@enter_password_input", password_1)
-      .setValue("@confirm_password_input", password_1)
-      .pause(1000)
-      .click("@clear_button")
-      .pause(1000)
-      .setValue("@create_wallet_input", "Wallet Test")
-      .setValue("@enter_password_input", password_1)
-      .setValue("@confirm_password_input", password_1)
-      .pause(1000)
-      .click("@create_button")
-      .pause(1000)
-      .click("@show_Private_Key_a")
-      .click('')
-      //.pause(1000)
-      //.click("@continue_a")
-      .pause()
-      //.end();
+      for (let i = 0; i < 100; i++) {
+        signinvar.click("@cat")
+      } 
+      
   },
 };
