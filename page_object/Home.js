e4279e2078eb37498e3af351f1150455a673a88c
jiaming <jiaminglim0828@gmail.com>
2021-08-16 18:54:41 +0800

    Revert "test popcat"
    This reverts commit c0f99b1083d6312333e1dd85d1b67171d1aa7a17.

--- page_object/Home.js
+++ page_object/Home.js
@@ -1,11 +1,39 @@
 
 module.exports = {
-  url : 'https://popcat.click/',
+  url : 'http://localhost:8080/',
   elements:{
-    cat:{
+    create_button:{
       locateStrategy: 'xpath',
-      selector: '//*[@id="app"]'
-    }
+      selector: '//button[text()="Create"]'
+    },
+    create_a:  {
+      locateStrategy: 'xpath',
+      selector: '//a[contains(text(),"Create")]',
+    },
+    clear_button:  {
+      locateStrategy: 'xpath',
+      selector: '//button[contains(text(),"Clear")]'
+    },
+    create_wallet_input:  {
+      locateStrategy: 'xpath',
+      selector: '//input[contains(@placeholder,"Wallet Name")]'
+    },
+    enter_password_input:  {
+      locateStrategy: 'xpath',
+      selector: '//input[contains(@placeholder,"Enter a New Password")]'
+    },
+    confirm_password_input:  {
+      locateStrategy: 'xpath',
+      selector: '//input[contains(@placeholder,"Confirm New Password")]'
+    },
+    Show_Private_Key_a:  {
+      locateStrategy: 'xpath',
+      selector: '//a[contains(text(),"Show Private Key")]',
+    },
+    Continue_a:  {
+      locateStrategy: 'xpath',
+      selector: '//a[contains(text(),"Continue")]',
+    },
   }
 
 }
--- test/Home_test.js
+++ test/Home_test.js
@@ -2,13 +2,32 @@ var password_1 = "abcd1234";
 var password_2 = "11111111";
 var password_3 = "11111";
 module.exports = {
-  "spam_click": function (browser) {
+  "create-wallet": function (browser) {
     var signinvar = browser.page.Home();
     signinvar
       .navigate()
-      for (let i = 0; i < 100; i++) {
-        signinvar.click("@cat")
-      } 
-      
+      .pause(1000)
+      .click("@create_a")
+      .pause(1000)
+      .click("@create_a")
+      .pause(1000)
+      .setValue("@create_wallet_input", "Wallet Test")
+      .setValue("@enter_password_input", password_1)
+      .setValue("@confirm_password_input", password_1)
+      .pause(1000)
+      .click("@clear_button")
+      .pause(1000)
+      .setValue("@create_wallet_input", "Wallet Test")
+      .setValue("@enter_password_input", password_1)
+      .setValue("@confirm_password_input", password_1)
+      .pause(1000)
+      .click("@create_button")
+      .pause(1000)
+      .click("@show_Private_Key_a")
+      .click('')
+      //.pause(1000)
+      //.click("@continue_a")
+      .pause()
+      //.end();
   },
 };
