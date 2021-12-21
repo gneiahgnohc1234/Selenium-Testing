55e5ade52df56455e22cfe4a48e25b2928afcf69
Shi-Hui-Khoo <shihui.khoo00@gmail.com>
2021-12-21 13:23:05 +0800

    updated test cases for create account from private key

--- page_object/Createaccount.js
+++ page_object/Createaccount.js
@@ -13,14 +13,24 @@ const elements = {
     //copypublickey_successful: 'body > div:nth-child(9) > div:nth-child(1) > div:nth-child(1)',
     createsuccessful_popup: '#app > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1)',
     input_accountname: '#app > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > input',
+    input_accountname_pk: '#app > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > div:nth-child(2) > div:nth-child(1) > input',
     input_walletpassword: '#app > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > div:nth-child(2) > div:nth-child(1) > input',
+    input_walletpassword_pk: '#app > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > div:nth-child(3) > div:nth-child(1) > input',
+    input_privatekey: '#app > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > input',
     home_icon: '#app > div:nth-child(1) > header > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > a > img',
     empty_accountname: '#app > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div.error.error-text.text-left.my-2',
+    empty_accountname_pk: '#app > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > div:nth-child(2) > div.error.error-text.text-left.my-2',
     empty_walletpassword: '#app > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > div:nth-child(2) > div.error.error-password.text-left.my-2',
+    empty_walletpassword_pk: '#app > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > div:nth-child(3) > div.error.error-password.text-left.my-2',
+    empty_privatekey: '#app > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div.error.error-password.text-left.my-2',
     error_existingname: '#app > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3)',
+    error_existingprivatekey: '#app > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3)',
+    error_invalidpk: '#app > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div.error.error-password.text-left.my-2',
     //enter_passwordpopup: '#app > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(4) > div:nth-child(12) > div:nth-child(2) > div:nth-child(3) > div:nth-child(1)',
     wrong_walletpassword: '#app > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3)',
     password_eyeicon: '#app > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(4) > div:nth-child(2) > div:nth-child(1) > svg > path',
+    password_eyeicon_pk: '#app > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(4) > div:nth-child(3) > div:nth-child(1) > svg > path',
+    privatekey_eyeicon: '#app > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(4) > div:nth-child(1) > div:nth-child(1) > svg > path',
     public_key: '#public',
     private_key: '#app > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(4) > div:nth-child(12) > div:nth-child(2) > div:nth-child(1)',
     //view_privatekey: '#app > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(4) > div:nth-child(12) > div:nth-child(2) > svg > path'
@@ -66,9 +76,22 @@ const commands = {
         })
     },
 
-    // empty_inputpkaccount(){
-
-    // },
+    empty_inputaccount_pk(){
+        return this
+        .click("@input_privatekey")
+        .setValue("@input_privatekey", "\ue004")
+        .isVisible('@empty_privatekey', callback = (result) => {
+            this.assert.equal(result.value, true, 'If private key has no input, error is shown')
+        })
+        .setValue("@input_accountname_pk", "\ue004")
+        .isVisible('@empty_accountname_pk', callback = (result) => {
+            this.assert.equal(result.value, true, 'If account name has no input, error is shown')
+        })
+        .setValue("@input_walletpassword_pk", "\ue004")
+        .isVisible('@empty_walletpassword_pk', callback = (result) => {
+            this.assert.equal(result.value, true, 'If wallet password has no input, error is shown')
+        })
+    },
 
     wrong_walletpassword(name, password){
         return this
@@ -81,12 +104,38 @@ const commands = {
         })
     },
 
+    wrong_walletpassword_pk(privatekey, name, password){
+        return this
+        .click("@input_privatekey")
+        .setValue("@input_privatekey", privatekey)
+        .setValue("@input_accountname_pk", name)
+        .setValue("@input_walletpassword_pk", password)
+        .click("@create")
+        .isVisible('@wrong_walletpassword', callback = (result) => {
+            this.assert.equal(result.value, true, 'If wallet password is wrong, error is shown')
+        })
+    },
+
     eye_icon(){
         return this
         .click('@password_eyeicon')
         .assert.elementPresent('@password_eyeicon', "When eye icon is clicked, password field is unmasked")
         .click('@password_eyeicon')
         .assert.elementPresent('@password_eyeicon', "When eye icon is clicked, password field is masked again")
+        .pause(2000)
+        .end()
+    },
+
+    eye_icon_pk(){
+        return this
+        .click('@privatekey_eyeicon')
+        .assert.elementPresent('@privatekey_eyeicon', "When eye icon is clicked, private key field is unmasked")
+        .click('@privatekey_eyeicon')
+        .assert.elementPresent('@privatekey_eyeicon', "When eye icon is clicked, private key field is masked again")
+        .click('@password_eyeicon_pk')
+        .assert.elementPresent('@password_eyeicon_pk', "When eye icon is clicked, password field is unmasked")
+        .click('@password_eyeicon_pk')
+        .assert.elementPresent('@password_eyeicon_pk', "When eye icon is clicked, password field is masked again")
         .pause(2000)
         .end()
     },
@@ -127,6 +176,31 @@ const commands = {
         // })
     },
 
+    create_account_pk(privatekey, name, password){
+        return this
+        .click("@input_privatekey")
+        .setValue("@input_privatekey", privatekey)
+        .setValue("@input_accountname_pk", name)
+        .setValue("@input_walletpassword_pk", password)
+        .click("@create")
+        .pause(1000)
+        .assert.visible('@createsuccessful_popup', 'Account is successfully created when account name is valid and wallet password is correct')
+        .click("@close")
+        .pause(1000)
+        .isVisible('@account_detailstab', callback = (result) => {
+            this.assert.equal(result.value, true, 'If account is successfully created, user is navigated to the newly created account details page')
+        })
+        .isVisible('@address', callback = (result) => {
+            this.assert.equal(result.value, true, 'If account is successfully created, wallet address is visible')
+        })
+        .isVisible('@public_key', callback = (result) => {
+            this.assert.equal(result.value, true, 'If account is successfully created, public key is visible')
+        })
+        .isVisible('@private_key', callback = (result) => {
+            this.assert.equal(result.value, true, 'If account is successfully created, private key is available')
+        })
+    },
+
     existing_name(name, password){
         return this
         .click("@create_account")
@@ -137,6 +211,43 @@ const commands = {
         .isVisible('@error_existingname', function callback(result) {
             this.assert.equal(result.value, true, 'If account name is taken, error is shown')
         })
+    },
+
+    existing_name_pk(privatekey, name, password){
+        return this
+        .click("@create_account")
+        .click("@createnew_frompk")
+        .setValue("@input_privatekey", privatekey)
+        .setValue("@input_accountname_pk", name)
+        .setValue("@input_walletpassword_pk", password)
+        .click("@create")
+        .isVisible('@error_existingname', function callback(result) {
+            this.assert.equal(result.value, true, 'If account name is taken, error is shown')
+        })
+    },
+
+    existing_privatekey(privatekey, name, password){
+        return this
+        .click("@create_account")
+        .click("@createnew_frompk")
+        .setValue("@input_privatekey", privatekey)
+        .setValue("@input_accountname_pk", name)
+        .setValue("@input_walletpassword_pk", password)
+        .click("@create")
+        .isVisible('@error_existingprivatekey', function callback(result) {
+            this.assert.equal(result.value, true, 'If account with that private key is taken, error is shown')
+        })
+        .pause(1000)
+    },
+
+    invalid_privatekey(privatekey){
+        return this
+        .click("@input_privatekey")
+        .setValue("@input_privatekey", privatekey)
+        .isVisible('@error_invalidpk', callback = (result) => {
+            this.assert.equal(result.value, true, 'If private key length is less than required minimum, error is shown')
+        })
+
     }
 
     
--- test/Createaccount_test.js
+++ test/Createaccount_test.js
@@ -1,6 +1,9 @@
 var name = "Selenium"
+var name2 = "Selenium2"
 var password = "abcd1234"
 var password2 = "abcd12345"
+var privatekey = '9FB0AC3CE2E5A1A8092EB7B73861F38DFE07768E6AD421CE14AE79B3203D862A'
+var privatekey1 = '9FB0AC3CE2E5A1A8092EB7B73861F38DFE07768E6AD421CE14AE79B3203D862'
 
 
 module.exports = {
@@ -23,13 +26,24 @@ module.exports = {
 
         //create new account
         account
+            // .navigate_createaccount(browser.launch_url)
+            // .navigate_createnewaccount(browser.launch_url)
+            // .empty_inputaccount()
+            // //.wrong_walletpassword(name, password2)
+            // //.eye_icon()
+            // .create_account(name, password)
+            // .existing_name(name, password)
+
             .navigate_createaccount(browser.launch_url)
-            .navigate_createnewaccount(browser.launch_url)
-            .empty_inputaccount()
-            //.wrong_walletpassword(name, password2)
-            //.eye_icon()
-            .create_account(name, password)
-            .existing_name(name, password)
+            .navigate_createnewfrompk(browser.launch_url)
+            .invalid_privatekey(privatekey1)
+            //.empty_inputaccount_pk()
+            //.wrong_walletpassword_pk(privatekey, name, password2)
+            //.eye_icon_pk()
+            //.create_account_pk(privatekey, name, password)
+            //.existing_name_pk(privatekey, name, password)
+            //.existing_privatekey(privatekey, name2, password)
+
     }
 
 
