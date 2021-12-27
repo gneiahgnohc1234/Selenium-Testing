84ed3a80012057bbc2525910372e974b16d56b27
Shi-Hui-Khoo <shihui.khoo00@gmail.com>
2021-12-27 14:52:06 +0800

    added test cases for address book: add new address successful, invalid, existing address validation, empty name validation

--- test/Addressbook_test.js
+++ test/Addressbook_test.js
@@ -1,25 +1,38 @@
-var address = "XANRRV-JWFLWR-4LITVY-JQG6YT-HHZNJ4-QGPY6F-V7IS";
+var address = "XANRRV-JWFLWR-4LITVY-JQG6YT-HHZNJ4-QGPY6F-V7IS"
 var address2 ='aaaaaa-aaaaaa-aaaaaa-aaaaaa-aaaaaa-aaaaaa-aaaa'
-var name = "Selenium";
-var password = "abcd1234";
+var address3 = 'XANRRV-JWFLWR-4LITVY-JQG6YT-HHZNJ4-QGPY6F-V7I'
+var address4 = 'XANRRV-JWFLWR-4LITVY-JQG6YT-HHZNJ4-QGPY6F-V7ISS'
+var name = "Selenium"
+var addressname = "Sully"
+var password = "abcd1234"
+var custom = "Father"
+
+
 module.exports = {
+
     "AddressBook_test": function (browser) {
-        var create = browser.page.Createwallet();
-        var signin = browser.page.Signin();
-        var addressbook = browser.page.Addressbook();
-        //create wallet
+
+        var create = browser.page.Createwallet()
+        var signin = browser.page.Signin()
+        var addressbook = browser.page.Addressbook()
+
+        // create wallet
         create
             .navigate()
-            .createwallet(name,password)
-        //signin
+            .navigate_createnewwallet(browser.launch_url)
+            .create_wallet(browser.launch_url, name, password)
+
+        // sign in
         signin
-            .signin(password)
+            .signin_dashboard(browser.launch_url, password)
+
         addressbook
-            .navigate_addcontacts(browser.launch_url)
-            .emptyfield_validation()
-            .clearbutton_validation(name, address)
-            .incorrectaddress_validation(name,address2)
-            .save_address(browser.launch_url,name,address)
+            .navigation_addressbook(browser.launch_url)
+            .add_newaddress(browser.launch_url, addressname, address, custom)
+            .existing_address(name, address)
+            // .invalid_address(address2, address3, address4)
+            // .empty_name(address)
 
     }
+
 }
