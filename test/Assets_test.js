8f6d363c625aeb2fe13c34b62bb59d88cbcf5b19
Shi-Hui-Khoo <shihui.khoo00@gmail.com>
2022-01-04 17:49:13 +0800

    added test case for assets: create asset successful and empty password validation

--- test/Assets_test.js
+++ test/Assets_test.js
@@ -1,8 +1,12 @@
 var name = "Selenium"
 var password = "abcd1234"
+var supply = "2"
+var divisibility = "6"
 // var privatekey = "449198BF93D383DD7F9769DD0ED212B8F28D4A5F2CFD3B282685C3707356DA0C"
-var privatekey1 = "449198BF93D383DD7F9769DD0ED212B8F28D4A5F2CFD3B282685C3707356DA0C"
-var privatekey2 = "AD947A8674BBB115DBC7C33351F808D3ED4C8F2DCBFAFA8C68861B3124CA4E26"
+// var privatekey1 = "449198BF93D383DD7F9769DD0ED212B8F28D4A5F2CFD3B282685C3707356DA0C"
+// var privatekey2 = "AD947A8674BBB115DBC7C33351F808D3ED4C8F2DCBFAFA8C68861B3124CA4E26"
+// var privatekey = "D1D60FD83033CB99886A31AEBF92BF82AB283DC2AFD65817B957AB13E6B3D3E8" // testing
+var privatekey = "0ACDDBF848D081613E665144FF48181EBE4E009D27F33C53AC32180D73A6C667" // new account
 
 module.exports = {
 
@@ -27,10 +31,11 @@ module.exports = {
         signin
             .signin_dashboard(browser.launch_url, password)
         
+        // asset test
         asset
             .navigation_assets(browser.launch_url)
             .empty_password()
-            .create_asset()
+            .create_asset(supply, divisibility, password)
 
     }
 
--- test/Namespace_test.js
+++ test/Namespace_test.js
@@ -1,12 +1,13 @@
 var name = "Selenium"
 var shortname = "s"
-var namespace_input = "sirius"
+var namespace_input = "noexistingname"
 var password = "abcd12345678"
-var privatekey1 = "449198BF93D383DD7F9769DD0ED212B8F28D4A5F2CFD3B282685C3707356DA0C" // has namespace in account
-var privatekey2 = "AD947A8674BBB115DBC7C33351F808D3ED4C8F2DCBFAFA8C68861B3124CA4E26" // has namespace in account
-var privatekey3 = "D1D60FD83033CB99886A31AEBF92BF82AB283DC2AFD65817B957AB13E6B3D3E8" // for testing, got error, namespace owner conflict
-var privatekey4 = "1C77F6CA8DD0865D9B5141B2745C5D09D0EB7FFFBA2DE4D075523DE8F5AAA0FC"
-var privatekey = "" // brand new account
+// var privatekey1 = "449198BF93D383DD7F9769DD0ED212B8F28D4A5F2CFD3B282685C3707356DA0C" // has namespace in account
+// var privatekey2 = "AD947A8674BBB115DBC7C33351F808D3ED4C8F2DCBFAFA8C68861B3124CA4E26" // has namespace in account
+// var privatekey3 = "D1D60FD83033CB99886A31AEBF92BF82AB283DC2AFD65817B957AB13E6B3D3E8" // for testing, got error, namespace owner conflict
+// var privatekey4 = "D455CDF321690411B45F013C7EE005BFD20DCDEDCF62C3AA1DCE096C86C5B795"
+// var privatekey = "" // brand new account
+var namespace_pk = "0697649924B896CD48B9628660EDC61E850F81E869F1830B5EAA05191523EAC6"
 
 module.exports = {
 
@@ -25,13 +26,14 @@ module.exports = {
         // create wallet
         create
             .navigate_createpkwallet(browser.launch_url)
-            .create_pkwallet(browser.launch_url, privatekey4, name, password)
+            // .create_pkwallet(browser.launch_url, privatekey4, name, password)
+            .create_pkwallet(browser.launch_url, namespace_pk, name, password)
 
         // sign in
         signin
             .signin_dashboard(browser.launch_url, password)
         
-        // namespace
+        // namespace test
         namespace
             .navigate_namespace(browser.launch_url)
             .invalid_name(shortname)
